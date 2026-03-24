7fe326eac93ad7bd132f12e2a822bcfd380296b4
Thomas <tc12game@gmail.com>
2026-03-24 14:23:04 -0400

    Can now do one request at a time.

--- scrape_web.py
+++ scrape_web.py
@@ -3,12 +3,13 @@ import requests
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
 from bs4 import BeautifulSoup
+import time
 
 # Get the Tax ID from the excel sheet
 
 df = pd.read_excel(r"C:\Users\tc13g\Downloads\Master Association List.xlsx")
 
-for index, row in df.head(1).iterrows():
+for index, row in df.head(10).iterrows():
     tax_id = str(row.iloc[4]).strip().replace("-", "")
     print(f"Checking row {index}, tax id = {tax_id}")
 
@@ -18,9 +19,7 @@ for index, row in df.head(1).iterrows():
         "searchTerm": tax_id
     }
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
-        "accept": '"text/html,application...',
-        "referer": 'https://...',
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"
     }
 
     query_string = urlencode(params)
@@ -30,5 +29,32 @@ for index, row in df.head(1).iterrows():
     web_byte = urlopen(request).read()
     webpage = web_byte.decode('utf-8')
 
-    print(webpage)
+    soup = BeautifulSoup(webpage, "html.parser")
+
+    for a in soup.find_all("a", href=True):
+        text = a.get_text(strip=True)
+        href = a["href"]
+
+        if text == tax_id:
+            detail_url = "https://search.sunbiz.org" + href
+
+            second_request = Request(detail_url, headers=headers)
+            second_web_byte = urlopen(second_request).read()
+            second_webpage = second_web_byte.decode('utf-8')
+
+            soup = BeautifulSoup(second_webpage, "html.parser")
+            annual_reports_text = soup.find(string="Annual Reports")
+            annual_reports_table = annual_reports_text.parent.find_next("table")
+            
+            if "2026" in annual_reports_table.get_text():
+                df.at[index, "2026 Annual Report"] = "Yes"
+            else:
+                df.at[index, "2026 Annual Report"] = "No"
+                
+            break
+    
+    df.to_excel(r"C:\Users\tc13g\Downloads\Master Association List.xlsx", index=False)
+    time.sleep(5)
+
+
 
